a11997d11e2e74d643812449c987ebc33485f671
Sean Edwards <stedwards87+git@gmail.com>
2016-02-19 15:08:07 -0500

    Cookbook updates for Consul UI

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 default[:consul][:config][:server] = true
+default[:consul][:config][:ui] = true
+default[:consul][:config][:ui_dir] = '/srv/consul-ui/current'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,9 @@
-#
-# Cookbook Name:: cfer-consul-cluster
-# Recipe:: default
-#
-# Copyright (C) 2016 YOUR_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 package 'jq'
 package 'awscli'
 
 chef_gem 'aws-sdk'
 
+# Consul requires an explicit bind address when running on EC2.
 node.default[:consul][:config][:bind_addr] = node['ec2']['local_ipv4'] if node.attribute?('ec2')
 
 include_recipe 'aws'
