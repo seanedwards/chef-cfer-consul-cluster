3325546e7e1c5a6bb08d35e2122f00ee974e7b42
Sean Edwards <stedwards87+git@gmail.com>
2016-02-19 15:09:33 -0500

    Consul leaves the cluster when it terminates

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default[:consul][:config][:server] = true
 default[:consul][:config][:ui] = true
 default[:consul][:config][:ui_dir] = '/srv/consul-ui/current'
+default[:consul][:config][:leave_on_terminate] = true
 
